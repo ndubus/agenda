103d83d40860362ea3e12bad6d2d3403ae6c304a
abdel792 <abdelkrim.bensaid@gmail.com>
2021-09-14 09:35:40 +0200

    Created the first release 1.0 by Nicolas, compatible until nvda-2020.4.

---
+++ addon/appModules/agenda.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#agenda.py
+import appModuleHandler
+import controlTypes
+import api
+import ui
+import speech
+
+fieldHeader = {
+10:"Mois",
+11:u"Année",
+12:"Heure",
+13:"Minute",
+14:u"Durée en minutes ou en heures",
+15:"Motif"
+}
+
+class AppModule(appModuleHandler.AppModule):
+	notSearch = 0
+
+	def event_gainFocus(self, obj, nextHandler):
+		controlID = obj.windowControlID	
+		if controlID in [9, 10, 11, 12, 13, 14, 15]:
+			if controlID==9:		
+				if obj.simplePrevious and obj.simplePrevious.simplePrevious and obj.simplePrevious.simplePrevious.name=="SUIVANT":
+					fieldHeader[9] = u"Jour de début"
+					self.notSearch = 1
+				elif obj.simpleNext and obj.simpleNext.simpleNext and obj.simpleNext.simpleNext.windowControlID==11 and (not  obj.simpleNext.simpleNext.simpleNext or obj.simpleNext.simpleNext.simpleNext.simpleNext and not obj.simpleNext.simpleNext.simpleNext.simpleNext.simpleNext):
+					fieldHeader[9]="Jour de fin" if self.notSearch else  "Jour de recherche"
+				else:
+					fieldHeader[9]=u"Jour du pense-bête" if (obj.simpleNext and obj.simpleNext.simpleNext and obj.simpleNext.simpleNext.simpleNext and not obj.simpleNext.simpleNext.simpleNext.simpleNext) or (obj.simpleNext and obj.simpleNext.simpleNext and obj.simpleNext.simpleNext.simpleNext and obj.simpleNext.simpleNext.simpleNext.simpleNext and obj.simpleNext.simpleNext.simpleNext.simpleNext.simpleNext and not obj.simpleNext.simpleNext.simpleNext.simpleNext.simpleNext.simpleNext) else "Jour du rendez-vous"
+			ui.message(fieldHeader[controlID])
+			speech.speakObject(obj, reason=controlTypes.REASON_CHANGE)
+		else:
+			speech.speakObject(obj, reason=controlTypes.REASON_FOCUS)
+		nextHandler
+
+	def script_searchDate (self, gesture):
+		gesture.send()
+		self.notSearch = 0
+
+	__gestures={
+		"kb:control+j":"searchDate"
+	}
---
+++ addon/doc/fr/readme.md
@@ -0,0 +1,16 @@
+# agenda
+
+* Auteurs : Nicolas Dubus, Abdelkrim Bensaïd
+
+# Présentation #
+
+Cette extension améliore l'accessibilité du programme agenda créé par Jean-Claude Mortimor avec NVDA.
+
+Les zones d'édition rencontrées lors de la création d'un rendez-vous ou d'un pense-bête, d'une liste d'un rendez-vous par semaine ou d'une liste de rendez-vous ont été étiquetées pour faciliter l'utilisation du programme.
+
+L'annonce de la description de l'objet est automatiquement activée dans les options de NVDA si cela n'est pas déjà le cas.
+
+## Compatibilité ##
+
+* Cette extension est compatible avec les versions de NVDA allant de la 2014.3 à la 2020.4.
+
