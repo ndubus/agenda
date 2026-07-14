b6b7bb22dc63767b529250cbc6c06203280db777
Abdel792 <abdelkrim.bensaid@gmail.com>
2026-07-14 22:34:26 +0200

    Set addon_info["addon_url"] to https://github.com/ndubus/agenda in buildsVars.py metadata

--- buildVars.py
+++ buildVars.py
@@ -29,7 +29,7 @@ addon_info = {
 	# Author(s)
 	"addon_author" : u"Nicolas DUBUS <nicolas.dubus@orange.fr>, Abdelkrim BENSAÏD",
 	# URL for the add-on documentation support
-	"addon_url" : "",
+	"addon_url" : "https://github.com/ndubus/agenda",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
